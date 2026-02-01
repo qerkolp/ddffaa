85be48f86b1b1e0ff707945fc384f4fce1adfd13
qerkolp <qulikcz@gmail.com>
2026-02-01 16:18:52 +0100

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg');
+import { Client } from 'pg';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send("Method Not Allowed");
@@ -52,4 +52,5 @@ export default async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
+
 }
